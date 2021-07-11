e25e20563ae0beeaa96ffdce6360b63de58258f4
Joanna Doyle <jjadoyle@gmail.com>
2021-07-11 06:30:32 +0200

    make monster killable

--- src/main.rs
+++ src/main.rs
@@ -7,67 +7,111 @@ use tui::widgets::{Widget, Block, Borders};
 use tui::layout::{Layout, Constraint, Direction};
 use termion::{async_stdin, AsyncReader};
 
-struct Character {
-    x: u16,
-    y: u16
-}
-
 struct Monster {
     x: u16,
     y: u16,
-    glyph: char
+    glyph: char,
+    hp: u16
+}
+
+impl Monster {
+    pub fn damage(&self) -> u16 {
+        return 10;
+    }
+}
+
+struct Character {
+    x: u16,
+    y: u16,
+    hp: u16
 }
 
 impl Character {
-    pub fn move_left(&mut self, mon: &Monster) {
+    pub fn fight(&mut self, mon: &mut Monster) {
+        let player_dmg = self.damage();
+        let mon_dmg = mon.damage();
+        if player_dmg > mon.hp {
+            mon.hp = 0;
+        } else {
+            mon.hp -= player_dmg;
+        }
+        if mon.hp > 0 {
+            self.hp -= mon_dmg;
+            if self.hp < 0 {
+                self.hp = 0;
+            }
+        }
+    }
+
+    pub fn damage(&self) -> u16 {
+        return 10;
+    }
+
+    pub fn move_left(&mut self, mon: &mut Monster) {
         if self.x - 1 != mon.x || self.y != mon.y {
             self.x -= 1;
+        } else {
+            self.fight(mon);
         }
     }
 
-    pub fn move_down(&mut self, mon: &Monster) {
+    pub fn move_down(&mut self, mon: &mut Monster) {
         if self.x != mon.x || self.y + 1 != mon.y {
             self.y += 1;
+        } else {
+            self.fight(mon);
         }
     }
 
-    pub fn move_up(&mut self, mon: &Monster) {
+    pub fn move_up(&mut self, mon: &mut Monster) {
         if self.x != mon.x || self.y - 1 != mon.y {
             self.y -= 1;
+        } else {
+            self.fight(mon);
         }
     }
 
-    pub fn move_right(&mut self, mon: &Monster) {
+    pub fn move_right(&mut self, mon: &mut Monster) {
         if self.x + 1 != mon.x || self.y != mon.y {
             self.x += 1;
+        } else {
+            self.fight(mon);
         }
     }
 
-    pub fn move_upleft(&mut self, mon: &Monster) {
+    pub fn move_upleft(&mut self, mon: &mut Monster) {
         if self.x - 1 != mon.x || self.y - 1 != mon.y {
             self.x -= 1;
             self.y -= 1;
+        } else {
+            self.fight(mon);
         }
     }
 
-    pub fn move_upright(&mut self, mon: &Monster) {
+    pub fn move_upright(&mut self, mon: &mut Monster) {
         if self.x + 1 != mon.x || self.y - 1 != mon.y {
             self.x += 1;
             self.y -= 1;
+        } else {
+            self.fight(mon);
         }
     }
 
-    pub fn move_downleft(&mut self, mon: &Monster) {
+    pub fn move_downleft(&mut self, mon: &mut Monster) {
         if self.x - 1 != mon.x || self.y + 1 != mon.y {
             self.x -= 1;
             self.y += 1;
+        } else {
+            self.fight(mon);
         }
     }
 
-    pub fn move_downright(&mut self, mon: &Monster) {
+    pub fn move_downright(&mut self, mon: &mut Monster) {
         if self.x + 1 != mon.x || self.y + 1 != mon.y {
             self.x += 1;
             self.y += 1;
+        } else {
+            self.fight(mon);
         }
     }
 }
@@ -78,8 +122,8 @@ fn main() -> Result<(), io::Error> {
     let mut terminal = Terminal::new(backend)?;
     let mut read_handle = async_stdin();
     let mut buf:[u8; 1024] = [0; 1024];
-    let mut player = Character { x: 10, y: 10 };
-    let mon = Monster { x: 4, y: 15, glyph: 'd' };
+    let mut player = Character { x: 10, y: 10, hp: 50 };
+    let mut mon = Monster { x: 4, y: 15, glyph: 'd', hp: 10 };
     loop {
         let mut bytes_read = read_handle.read(&mut buf)?;
         print!("{}", termion::clear::All);
@@ -87,21 +131,25 @@ fn main() -> Result<(), io::Error> {
             let byte = buf[bytes_read - 1];
             bytes_read -= 1;
             match byte {
-                b'h' => player.move_left(&mon),
-                b'j' => player.move_down(&mon),
-                b'k' => player.move_up(&mon),
-                b'l' => player.move_right(&mon),
-                b'y' => player.move_upleft(&mon),
-                b'u' => player.move_upright(&mon),
-                b'b' => player.move_downleft(&mon),
-                b'n' => player.move_downright(&mon),
+                b'h' => player.move_left(&mut mon),
+                b'j' => player.move_down(&mut mon),
+                b'k' => player.move_up(&mut mon),
+                b'l' => player.move_right(&mut mon),
+                b'y' => player.move_upleft(&mut mon),
+                b'u' => player.move_upright(&mut mon),
+                b'b' => player.move_downleft(&mut mon),
+                b'n' => player.move_downright(&mut mon),
                 b'q' => process::exit(0),
                 _ => (),
             }
         }
-        print!("{}@", termion::cursor::Goto(player.x, player.y));
-        print!("{}{}", termion::cursor::Goto(mon.x, mon.y), mon.glyph);
-        print!("{}", termion::cursor::Goto(player.x, player.y));
+        if player.hp > 0 {
+            print!("{}@", termion::cursor::Goto(player.x, player.y));
+            if mon.hp > 0 {
+                print!("{}{}", termion::cursor::Goto(mon.x, mon.y), mon.glyph);
+            }
+            print!("{}", termion::cursor::Goto(player.x, player.y));
+        }
         terminal.flush();
     }
     Ok(())
